62349d76a73ac88f0d35a364cc1277cd96adfaf2
Chris Wilson <christopher.e.wilson@gmail.com>
2013-03-05 14:17:38 -0500

    Updated for 2012 projections

--- scripts/build.py
+++ scripts/build.py
@@ -7,10 +7,13 @@ def dict_factory(cursor, row):
         d[col[0]] = row[idx]
     return d
 
+dir = os.getcwd()
 
-dir = os.getcwd() + "/"
-
-url = "http://www.census.gov/population/projections/files/downloadables/NP2008_D1.csv"
+def fetch_data():
+    url = "http://www.census.gov/population/projections/files/downloadables/NP2012_D1.csv"
+    f = open(dir + "/data/projections.csv", 'w')
+    f.write(urllib.urlopen(url).read())
+    f.close()
 
 #CSV format: HISP,RACE,SEX,YEAR,TOTAL_POP,POP_[0-99],
 #Description: http://www.census.gov/population/projections/files/downloadables/filelayout_NP2008_D1.txt
@@ -21,39 +24,32 @@ SEX  = [ "Both", "Male", "Female" ]
 def get_data():
     no = 0
     data = []
-    
-    page = urllib.urlopen(url).read()
-    #print page
-    
-    projs = re.split("\n", page)
-    print len(projs)
+        
+    projs = open(dir + "/data/projections.csv", "r").readlines()
     
     for line in projs:
         line = line.split(",")
-        if no > 1 and len(line) > 1: #first line is labels   
+        if no > 0 and len(line) > 1: #first line is labels   
             voting_age = 0
             for i in range(24, len(line)):
                 voting_age += int(line[i])
-
-            all_ages = ",".join(line[5:])
+                
         
-            c.execute('''INSERT OR IGNORE INTO projections (year, hispanic, race, sex, total, voting, all_ages) VALUES (?,?,?,?,?,?,?)''',
-                (int(line[3]), HISP[int(line[0])], RACE[int(line[1])], SEX[int(line[2])], int(line[4]), voting_age, all_ages))
-        no += 1
-        if no % 100 == 0:
+            c.execute('''INSERT OR IGNORE INTO projections (year, hispanic, race, sex, total, voting) VALUES (?,?,?,?,?,?)''',
+                        (int(line[3]), HISP[int(line[0])], RACE[int(line[1])], SEX[int(line[2])], int(line[4]), voting_age))
             conn.commit()
-            print no
-    conn.commit()
+        no += 1
         
-def get_year(y):
+def get_year(y, ind="voting"):
     return {
-        'white': c.execute('''SELECT * from projections WHERE hispanic='Not Hispanic' AND race = 'White' AND sex = "Both" AND year = %i''' % y).fetchone()['voting'],
-        'black': c.execute('''SELECT * from projections WHERE hispanic='Not Hispanic' AND race = 'Black' AND sex = "Both" AND year = %i''' % y).fetchone()['voting'],
-        'asian': c.execute('''SELECT * from projections WHERE hispanic='Not Hispanic' AND race = 'Asian' AND sex = "Both" AND year = %i''' % y).fetchone()['voting'],
-        'hispanic': c.execute('''SELECT * from projections WHERE hispanic='Hispanic' AND race = 'All' AND sex = "Both" AND year = %i''' % y).fetchone()['voting']
+        'white': c.execute('''SELECT * from projections WHERE hispanic='Not Hispanic' AND race = 'White' AND sex = "Both" AND year = %i''' % y).fetchone()[ind],
+        'black': c.execute('''SELECT * from projections WHERE hispanic='Not Hispanic' AND race = 'Black' AND sex = "Both" AND year = %i''' % y).fetchone()[ind],
+        'asian': c.execute('''SELECT * from projections WHERE hispanic='Not Hispanic' AND race = 'Asian' AND sex = "Both" AND year = %i''' % y).fetchone()[ind],
+        'hispanic': c.execute('''SELECT * from projections WHERE hispanic='Hispanic' AND race = 'All' AND sex = "Both" AND year = %i''' % y).fetchone()[ind],
+        'total': c.execute('''SELECT * from projections WHERE hispanic='Total' AND race = 'All' AND sex = "Both" AND year = %i''' % y).fetchone()[ind]
     }        
 
-conn = sqlite3.connect(dir + 'projections.sqlite')
+conn = sqlite3.connect(dir + '/data/projections.sqlite')
 conn.row_factory = dict_factory
 c = conn.cursor()
 
@@ -65,23 +61,27 @@ c.execute('CREATE TABLE IF NOT EXISTS projections \
             "sex" VARCHAR(10), \
             "total" INTEGER, \
             "voting" INTEGER, \
-            "all_ages" TEXT, \
             CONSTRAINT "unq" UNIQUE (year, hispanic, race, sex))')
+
 conn.commit()
 
 def write_data():
-    f = open(dir + "demographics.csv", "w")
-    f.write("year,white,black,hispanic,asian\r\n")
-    i = 2000
-    while i <= 2048:
-        d = get_year(i)
-        f.write("%i,%i,%i,%i,%i\r\n" % (i,d['white'],d['black'],d['hispanic'],d['asian']))
-        i += 4
-    f.close()
+    f = open(dir + "/data/population.csv", "w")
+    f.write("year,white,black,hispanic,asian,total\r\n")
     
-get_data()
+    i = 2012
+    
+    while i <= 2060:
+        print i
+        d = get_year(i, "voting")
+        f.write("%i,%i,%i,%i,%i,%i\r\n" % (i,d['white'],d['black'],d['hispanic'],d['asian'],d['total']))
+        i += 1
+    
+    f.close()
+
+
+#fetch_data()
+#get_data()
 write_data()
 
-conn.commit()
 conn.close()
-
